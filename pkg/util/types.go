b1e5c077cd30f2969ea70c47c076723c65a88491
estoneman <elstoneman11@gmail.com>
2024-09-05 06:28:06 -0500

    export config struct

--- pkg/util/types.go
+++ pkg/util/types.go
@@ -8,9 +8,9 @@ import (
 type CustomURL url.URL // want to be able to do this: url.Print()
 
 type Config struct {
-	pages              map[string]int
-	baseURL            *url.URL
-	mu                 *sync.Mutex
-	concurrencyControl chan struct{}
-	wg                 *sync.WaitGroup
+	Pages              map[string]int
+	BaseURL            *url.URL
+	Mu                 *sync.Mutex
+	ConcurrencyControl chan struct{}
+	Wg                 *sync.WaitGroup
 }
