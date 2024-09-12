2394e32cb641d5128f5ce8fb6d73c9dfd43444c5
estoneman <elstoneman11@gmail.com>
2024-09-12 06:30:05 -0500

    add reporting functionality and maxPages field

--- pkg/util/types.go
+++ pkg/util/types.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"fmt"
 	"net/url"
+	"os"
 	"sync"
 )
 
@@ -9,8 +11,21 @@ type CustomURL url.URL // want to be able to do this: url.Print()
 
 type Config struct {
 	Pages              map[string]int
+	MaxPages           int64
 	BaseURL            *url.URL
 	Mu                 *sync.Mutex
 	ConcurrencyControl chan struct{}
 	Wg                 *sync.WaitGroup
+}
+
+func (cfg *Config) PrintReport(baseURL string) {
+	fmt.Fprintf(
+		os.Stderr,
+		"=============================\n  REPORT for %s\n=============================\n",
+		baseURL,
+	)
+
+	for url, freq := range cfg.Pages {
+		fmt.Printf("Found %d internal links to %s\n", freq, url)
+	}
 }
